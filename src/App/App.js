c68a8f51a725d0a65ec0a7c5c962d9338837ebd7
thechutrain <chu.chuyalan@gmail.com>
2017-06-09 19:40:06 -0400

    cleaning up the app

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import { Container } from 'reactstrap'
 // ==== other components ======
 import Expenses from '../Container/Expenses/Expenses.jsx'
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to Budge Tracker</h2>
+			<Container>
+				<div className="App">
+					<div className="App-header">
+						{/* <img src={logo} className="App-logo" alt="logo" /> */}
+						<h2>Welcome to Budge Tracker</h2>
+					</div>
+					<Expenses />
 				</div>
-				<Expenses />
-			</div>
+			</Container>
 		)
 	}
 }
