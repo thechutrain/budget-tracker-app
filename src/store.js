455f65fc8d45cbe11a8d572886b4c76b6ef29964
thechutrain <chu.chuyalan@gmail.com>
2017-06-10 16:04:00 -0400

    use compose to applymiddleware & add redux-dev tools

--- src/store.js
+++ src/store.js
@@ -1,8 +1,23 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
 import reducers from './reducers'
 
-const store = createStore(reducers, applyMiddleware(thunk, createLogger()))
+let store
+
+if (process.env.NODE_ENV === 'production') {
+	store = createStore(reducers, applyMiddleware(thunk, createLogger()))
+} else {
+	store = createStore(
+		reducers,
+		compose(
+			applyMiddleware(thunk, createLogger()),
+			typeof window === 'object' &&
+				typeof window.devToolsExtension !== 'undefined'
+				? window.devToolsExtension()
+				: f => f
+		)
+	)
+}
 
 export default store
