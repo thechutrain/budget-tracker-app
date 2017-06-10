59eb83e8fb347bec42b41cfb8c855178f93e0c40
thechutrain <chu.chuyalan@gmail.com>
2017-06-10 16:30:03 -0400

    added a basic userReducer file and added it to combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import expenseReducer from './expenseReducer'
+import userReducer from './userReducer'
 
 const reducers = combineReducers({
-	expenses: expenseReducer
+	expenses: expenseReducer,
+	userSettings: userReducer
 })
 
 export default reducers
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,16 @@
+// ========SAMPLE userSettings structure====
+// userSettings: {
+//   name: 'Alan',
+//   categories: [
+//     { type: 'Grocery', monthlyBudget: 125.75},
+//     { type: 'Dinning', monthlyBudget: 120}
+//   ]
+// }
+
+// ======= Actual Reducer ============
+function userReducer(state = {}, action) {
+	// switch statement on action.type
+	return state
+}
+
+export default userReducer
