0d5132a6122b1b52a11826c1040a0db73923559f
thechutrain <chu.chuyalan@gmail.com>
2017-06-10 16:10:23 -0400

    got rid of the container component and just made a div with class of container in my index.html

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Container } from 'reactstrap'
 // ==== other components ======
 import Expenses from '../Container/Expenses/Expenses.jsx'
 
 class App extends Component {
 	render() {
 		return (
-			<Container>
-				<div className="App">
-					<div className="App-header">
-						{/* <img src={logo} className="App-logo" alt="logo" /> */}
-						<h2>Welcome to Budge Tracker</h2>
-					</div>
-					<Expenses />
+			<div className="App">
+				<div className="App-header">
+					{/* <img src={logo} className="App-logo" alt="logo" /> */}
+					<h2>Welcome to Budge Tracker</h2>
 				</div>
-			</Container>
+				<Expenses />
+			</div>
 		)
 	}
 }
